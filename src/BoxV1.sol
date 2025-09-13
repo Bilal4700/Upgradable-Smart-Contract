a0912db518f33ce4b0563c2809f74419690d4fe2
Muhammad Bilal <bilal119797@gmail.com>
2025-09-13 05:13:43 -0600

    Initialse foundry built a upgradable smart contract structure

---
+++ src/BoxV1.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract BoxV1 is UUPSUpgradeable {
+    uint256 internal number;
+
+    function getNumber() external view returns (uint256) {
+        return number;
+    }
+
+    function Version() external pure returns (uint256) {
+        return 1;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override {}
+}
